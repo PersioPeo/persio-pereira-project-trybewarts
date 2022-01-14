3601ae34a1328a51a597b71c10e2091908f4c654
Pérsio Pereira <persiopeo@gmail.com>
2022-01-13 22:35:26 -0300

    Atualizando minha branch

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+// Ao preencher o formulário e clicar no botão, será validado que:
+// Caso o email seja "tryber@teste.com" e a senha seja "123456" será emitido um alerta contendo o texto "Olá, Tryber!"
+// Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos."
+
+// let buttonLogin = document.querySelector(".btn-entrar");
+//  buttonLogin.addEventListener('click', (event) => {
+//     event.preventDefault();
+//  });
+
+// function login() {
+//     let email = document.querySelector('[name=email]');
+//     let senha = document.querySelector('[name=password]');
+//     let userEmail = email.value;
+//     let userPassword = senha.value;
+//     let validEmail = 'tryber@teste.com';
+//     let validPassword = 123456;
+//     if(userEmail === validEmail & userPassword === validPassword) {
+//         console.log("Olá, Tryber!");
+//     } else {
+//         console.log("Email ou senha inválidos.")
+//     }
+// }
+
+// buttonLogin.addEventListener("click", login());
+
+/*-------------------------------------------------------------------------*/
 document.getElementById('button-header').addEventListener('click', () => {
-  const email = document.getElementById('i-email').value;
+  const email = document.getElementById('input-email').value;
   const senha = document.getElementById('input-senha').value;
   if (email === 'tryber@teste.com' && senha === '123456') {
     alert('Olá, Tryber!');
@@ -8,7 +34,7 @@ document.getElementById('button-header').addEventListener('click', () => {
   }
 });
 
-const botao = document.getElementById('submit-btn');
+const botao = document.getElementById('button-main');
 botao.disabled = true;
 document.getElementById('agreement').addEventListener('click', () => {
   if (document.getElementById('agreement').value === '') {
@@ -17,5 +43,3 @@ document.getElementById('agreement').addEventListener('click', () => {
     botao.disabled = false;
   }
 });
-
-// Atualizando minha branch
